f0374cd333fae3cbec4562dac2f5046f59e7b30c
riccardobon <riccardo.bontempo@gmail.com>
2021-01-27 11:48:37 +0100

    Refactored test and plot code

--- misc/unimodal_plot.R
+++ misc/unimodal_plot.R
@@ -3,22 +3,17 @@ library(dplyr)
 library(ggplot2)
 
 l <- list(12.3, 12.2, 11.499, 10.87, 8.7, 7, 6.9, 6.39, 5.2, 4.44, 3,
-	2.62, 2.48, 2.78, 2.891, 3.2, 3.3, 3.4, 5, 6, 8.5, 8.99, 11.32,
-	11.74)
+    2.62, 2.48, 2.78, 2.891, 3.2, 3.3, 3.4, 5, 6, 8.5, 8.99, 11.32,
+    11.74)
 
-y <- do.call(rbind.data.frame, l)
-
-df <- data.frame(1:nrow(y), y)
-
-names(df) = list("x", "y")
-
+df <- data.frame(x = seq_len(length(l)), y = do.call(rbind, l))
 df
 
-unimodal_plot <- df %>% ggplot(aes(x=x, y=y)) +
-	geom_point(size=3, color="red") +
-	geom_line() +
-	xlab("sequence") +
-	ylab("values") +
-	labs(title = "Example sequence", subtitle = waiver())
+unimodal_plot <- df %>% ggplot(aes(x = x, y = y)) +
+    geom_point(size = 3, color = "red") +
+    geom_line() +
+    xlab("sequence") +
+    ylab("values") +
+    labs(title = "Example sequence")
 
 ggsave("unimodal.png", plot = unimodal_plot)
--- test/unit/gss_test.py
+++ test/unit/gss_test.py
@@ -3,7 +3,7 @@ import unittest
 from gss_module.gss import GoldenSectionSearch
 
 
-class GoldenTestCase(unittest.TestCase):
+class GoldenSectionSearchTestCase(unittest.TestCase):
     """ Tests for GoldenSectionSearch"""
 
     def setUp(self) -> None:
@@ -23,18 +23,13 @@ class GoldenTestCase(unittest.TestCase):
         self.assertIsNone(self.gss.run([123.3, 456.6]))
 
     def test_not_none_on_three_elements_list(self) -> None:
-        """Test for not returning None if list has at least three elements"""
+        """Test for not returning None if list has three elements"""
         self.assertIsNotNone(self.gss.run([123.3, 456.6, 789.9]))
 
-    def test_minimum_implicit_on_positive_ints(self) -> None:
-        """Test for minimum on positive integers, with no optional extremum
-        parameter"""
+    def test_minimum_on_positive_ints(self) -> None:
+        """Test for minimum on positive integers"""
         self.assertEqual(self.gss.run([78, 23, 22, 21, 10, 4, 0, 1, 3, 11, 12,
                                        145]), 0)
-
-    def test_minimum_explicit_on_positive_ints(self) -> None:
-        """Test for minimum on positive integers, with optional extremum
-        parameter specified"""
         self.assertEqual(self.gss.run([787, 786, 4, 2, 1, 3, 11, 12, 13],
                                       True), 1)
 
@@ -43,14 +38,9 @@ class GoldenTestCase(unittest.TestCase):
         self.assertEqual(self.gss.run([3, 23, 122, 2100, 2101, 2099, 150,
                                       11, 14, 0], False), 2101)
 
-    def test_minimum_implicit_on_negative_ints(self) -> None:
-        """Test for minimum on negative integers, with no optional extremum
-        parameter"""
+    def test_minimum_on_negative_ints(self) -> None:
+        """Test for minimum on negative integers"""
         self.assertEqual(self.gss.run([-1, -2, -3, -10, -9, -3, -1]), -10)
-
-    def test_minimum_explicit_on_negative_ints(self) -> None:
-        """Test for minimum on negative integers, with optional extremum
-        parameter specified"""
         self.assertEqual(self.gss.run([-1, -2, -3, -10, -123, -122, -9, -3,
                                       -1], True), -123)
 
@@ -59,15 +49,10 @@ class GoldenTestCase(unittest.TestCase):
         self.assertEqual(self.gss.run([-44, -43, -22, -21, -11, -10, -11, -12,
                                       -100], False), -10)
 
-    def test_minimum_implicit_on_ints(self) -> None:
-        """Test for minimum on integers, with no optional extremum
-        parameter"""
+    def test_minimum_on_ints(self) -> None:
+        """Test for minimum on integers"""
         self.assertEqual(self.gss.run([30, 1, 0, -1, -2, -3, -10, -9, -3, -1,
                                       1, 1000]), -10)
-
-    def test_minimum_explicit_on_ints(self) -> None:
-        """Test for minimum on integers, with optional extremum parameter
-        specified"""
         self.assertEqual(self.gss.run([30, 1, 0, -1, -10, -900, -3, -1, 1,
                                       20], True), -900)
 
@@ -76,17 +61,13 @@ class GoldenTestCase(unittest.TestCase):
         self.assertEqual(self.gss.run([-30, -1, 0, 1, 10, 900, -3, -1000,
                                       -20000], False), 900)
 
-    def test_minimum_implicit_on_floats(self) -> None:
-        """Test for minimum on floats, with no optional extremum parameter"""
+    def test_minimum_on_floats(self) -> None:
+        """Test for minimum on floats"""
         self.assertEqual(self.gss.run([12.3, 12.2, 12.19999, 3, 2.9999, 3.0,
                                       3.1, 3.2, 3.3, 3.4, 5, 66, 777, 888]),
                          2.9999)
-
-    def test_minimum_explicit_on_floats(self) -> None:
-        """Test for minimum on floats, with optional extremum parameter
-        specified"""
         self.assertEqual(self.gss.run([12.3, 12.2, 12.19999, 3, 2.9999, -3.0,
-                                      -2.99999, 0, 2.2, 1.3, 3.4, 5], True),
+                                      -2.99999, 0, 2.2, 2.3, 3.4, 5], True),
                          -3.0)
 
     def test_maximum_on_floats(self) -> None:
@@ -95,14 +76,10 @@ class GoldenTestCase(unittest.TestCase):
                                       -0.00002, -0.00003, -0.00004], False),
                          0.00001)
 
-    def test_minimum_implicit_on_chars(self) -> None:
-        """Test for minimum on chars, with no optional extremum parameter"""
+    def test_minimum_on_chars(self) -> None:
+        """Test for minimum on chars"""
         self.assertEqual(self.gss.run(['h', 'f', 'e', 'c', 'b', 'i', 'j', 'k',
                                       'v', 'w', 'z']), 'b')
-
-    def test_minimum_explicit_on_chars(self) -> None:
-        """Test for minimum on chars, with optional extremum parameter
-        specified"""
         self.assertEqual(self.gss.run(['h', 'f', 'e', 'c', 'b', 'a', 'b', 'c',
                                       'd', 'j', 'z'], True), 'a')
 
@@ -111,15 +88,11 @@ class GoldenTestCase(unittest.TestCase):
         self.assertEqual(self.gss.run(['a', 'b', 'c', 'd', 'e', 'f', 't', 'r',
                                       'g', 'd', 'b'], False), 't')
 
-    def test_minimum_implicit_on_strings(self) -> None:
-        """Test for minimum on strings, with no optional extremum parameter"""
+    def test_minimum_on_strings(self) -> None:
+        """Test for minimum on strings"""
         self.assertEqual(self.gss.run(['strawberry', 'kiwi', 'cherry',
                                       'blueberry', 'banana', 'apple',
                          'avocado', 'mango', 'orange', 'pear']), 'apple')
-
-    def test_minimum_explicit_on_strings(self) -> None:
-        """Test for minimum on strings, with optional extremum parameter
-        specified"""
         self.assertEqual(self.gss.run(['sweden', 'spain', 'france', 'canada',
                                       'china', 'finland', 'japan',
                          'new zealand'], True), 'canada')
